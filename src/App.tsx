6ebf2cd14de036fb53b98b93d6509cc23a009878
arx-8 <swans.randomness@gmail.com>
2019-05-19 07:39:21 +0900

    Install emotion-js

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
+/** @jsx jsx */
 import React from "react"
 import logo from "./logo.svg"
-import "./App.css"
+import { css, jsx } from "@emotion/core"
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div css={root}>
+      <header css={header}>
+        <img src={logo} css={logoCss} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          css={link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -22,5 +23,39 @@ const App: React.FC = () => {
     </div>
   )
 }
+
+const root = css`
+  text-align: center;
+`
+
+const header = css`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+
+const logoCss = css`
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  animation: App-logo-spin infinite 20s linear;
+  height: 40vmin;
+  pointer-events: none;
+`
+
+const link = css`
+  color: #61dafb;
+`
 
 export default App
