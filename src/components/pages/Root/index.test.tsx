9b67de1cb055114d3f39e2b5df6395459e0066cb
arx-8 <swans.randomness@gmail.com>
2020-02-11 19:29:09 +0900

    test: Fix rendering test

--- src/components/pages/Root/index.test.tsx
+++ src/components/pages/Root/index.test.tsx
@@ -1,10 +1,15 @@
 import { render } from "@testing-library/react"
 import React from "react"
+import { MockProviders } from "src/components/helpers/MockProviders"
 
 import { Root } from "."
 
 test("renders learn react link", () => {
-  const { getByText } = render(<Root />)
+  const { getByText } = render(
+    <MockProviders>
+      <Root />
+    </MockProviders>
+  )
   const linkElement = getByText(/learn react/i)
   expect(linkElement).toBeInTheDocument()
 })
