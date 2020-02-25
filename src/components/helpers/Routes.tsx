e1e1fee8b3eab7c6a006768798ce1f27e8dd3a4f
arx-8 <swans.randomness@gmail.com>
2020-02-26 00:45:08 +0900

    feat: No hash router

--- src/components/helpers/Routes.tsx
+++ src/components/helpers/Routes.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { HashRouter, Route, Switch } from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 import { Diff } from "src/components/pages/Diff"
 import { NotFound } from "src/components/pages/NotFound"
 import { Root } from "src/components/pages/Root"
@@ -11,7 +11,7 @@ type OwnProps = {
 
 export const Routes: React.FC<OwnProps> = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Switch>
         <Route component={Root} exact path={ReactRouterPath.Root} />
         <Route component={Diff} exact path={ReactRouterPath.Diff} />
@@ -20,6 +20,6 @@ export const Routes: React.FC<OwnProps> = () => {
         <Route component={NotFound} path={ReactRouterPath.NotFound} />
         <Route component={NotFound} />
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
