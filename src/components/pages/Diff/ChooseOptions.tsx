eb6ca838085919b1b4d7ea37d96b086fee730fed
arx-8 <swans.randomness@gmail.com>
2021-03-14 12:08:14 +0900

    fix: Remove unnecessary collapse

--- src/components/pages/Diff/ChooseOptions.tsx
+++ src/components/pages/Diff/ChooseOptions.tsx
@@ -1,11 +1,8 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { css, jsx } from "@emotion/react"
-import { Collapse, Typography } from "@material-ui/core"
-import ExpandLessIcon from "@material-ui/icons/ExpandLess"
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
-import React, { useState } from "react"
-import { IconButtonGA } from "src/components/atoms/IconButtonGA"
+import { Typography } from "@material-ui/core"
+import React from "react"
 import { Checkboxes } from "src/components/molecules/Checkboxes"
 import { RadioButtons } from "src/components/molecules/RadioButtons"
 import {
@@ -30,54 +27,39 @@ export const ChooseOptions: React.FC<OwnProps> = ({
   setDiffMode,
   setDiffOptions,
 }) => {
-  const [isOpen, setIsOpen] = useState(false)
-
   return (
     <div css={root}>
       <div css={header}>
         <Typography variant="h6">Diff options</Typography>
-        <IconButtonGA
-          css={openBtn}
-          gaData={{
-            dataEventAction: "toggleOpenOptions",
-            dataEventCategory: "Bookmarkable Diff",
-            dataOn: "click",
-          }}
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          {isOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-        </IconButtonGA>
       </div>
 
-      <Collapse in={isOpen}>
-        <div>
-          <RadioButtons
-            exCss={radios}
-            onChange={setDiffMode}
-            selectedValue={diffMode}
-            values={DiffModeValues}
-          />
-        </div>
-        <div>
-          <Checkboxes
-            onChange={(nextValues) => {
-              setDiffOptions(
-                DiffOptionKeys.reduce((acc, curr) => {
-                  acc[curr] = nextValues.includes(curr)
-                  return acc
-                }, {} as DiffOptions)
-              )
-            }}
-            selectedValues={Object.entries(diffOptions)
-              .filter(([, v]) => !!v)
-              .map(([k]) => k as DiffOptionKey)}
-            values={DiffOptionKeys.map((k) => ({
-              label: k,
-              value: k,
-            }))}
-          />
-        </div>
-      </Collapse>
+      <div>
+        <RadioButtons
+          exCss={radios}
+          onChange={setDiffMode}
+          selectedValue={diffMode}
+          values={DiffModeValues}
+        />
+      </div>
+      <div>
+        <Checkboxes
+          onChange={(nextValues) => {
+            setDiffOptions(
+              DiffOptionKeys.reduce((acc, curr) => {
+                acc[curr] = nextValues.includes(curr)
+                return acc
+              }, {} as DiffOptions)
+            )
+          }}
+          selectedValues={Object.entries(diffOptions)
+            .filter(([, v]) => !!v)
+            .map(([k]) => k as DiffOptionKey)}
+          values={DiffOptionKeys.map((k) => ({
+            label: k,
+            value: k,
+          }))}
+        />
+      </div>
     </div>
   )
 }
@@ -87,10 +69,6 @@ const root = css``
 const header = css`
   display: flex;
   align-items: center;
-`
-
-const openBtn = css`
-  padding: 8px !important;
 `
 
 const radios = css`
